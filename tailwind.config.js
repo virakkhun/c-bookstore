ef43b8ad0163a7f63563ad36ec5ea397418af37b
virakkhun <rakmanoban@gmail.com>
2022-06-08 22:23:19 +0700

    add: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3730a3",
+        midPrimary: "##818cf8",
+        secondary: "#e5e5e5",
+        danger: "#dc2626",
+        info: "#84cc16",
+        dark: "#18181b",
+        midDark: "#1f2937",
+      },
+    },
   },
   plugins: [],
-}
+};
