f411e486aa47467af5303290150c6bae38adb157
Sokchea_Leang <leang.sokchea13@gmail.com>
2022-06-09 22:37:32 +0700

    nav and footer

--- src/components/layouts/_footer.vue
+++ src/components/layouts/_footer.vue
@@ -5,7 +5,14 @@ const title = ref("This is footer");
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center">{{ title.toUpperCase() }}</h1>
-  </div>
+  <footer class="w-full bg-white bottom-0 p-6 md:items-center">
+    <div class="md:px-5 px-5 container mx-auto">
+      <div class="flex items-center justify-center">
+        <div class="flex items-center">
+          <img src="https://logodownload.org/wp-content/uploads/2021/10/meta-logo-4.png" class="mr-5" width="70" alt="">
+          <p class="text-center text-sm items-center">Ⓒ 2022 CopyRight By Young Dev</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
--- src/components/layouts/_header.vue
+++ src/components/layouts/_header.vue
@@ -2,10 +2,46 @@
 import { ref } from "vue";
 
 const title = ref("This is header");
+const IconName = ref('menu')
+const toggleHam = (i : string) => {
+  i==='menu' ? IconName.value = 'close' : IconName.value = 'menu';
+}
+
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center">{{ title.toUpperCase() }}</h1>
-  </div>
+  <nav class="w-full bg-white  p-6 md:items-center">
+    <div class="md:px-5 px-5 container mx-auto">
+      <div class="flex">
+        <div class="flex-auto">
+          <img src="https://logodownload.org/wp-content/uploads/2021/10/meta-logo-4.png" width="120" alt="">
+        </div>
+        <div class="flex md:hidden text-3xl">
+          <ion-icon :name="IconName" @click="toggleHam(IconName)"></ion-icon>
+        </div>
+        <div class="md:flex-auto">
+          <ul class="md:flex md:items-center z-[-1] justify-end gap-10 md:z-auto md:static absolute w-full md:w-auto md:py-0 py-7 md:pl-0 pl-7 bg-white left-0 md:opacity-100"
+          :class="IconName==='menu' ? 'opacity-0' : 'opacity-100'">
+            <li class="mx-4 md:mx-0 md:my-0 my-6">
+              <router-link to="/" class="text-base hover:text-primary duration-300">Home</router-link>
+            </li>
+            <li  class="mx-4 md:mx md:my-0 my-6">
+              <router-link to="/book" class="text-base hover:text-primary duration-300">Book</router-link>
+            </li>
+            <li class="mx-4 md:mx-0 md:my-0 my-6">
+              <router-link to="login_register" class="text-base hover:text-primary duration-300">About Us</router-link>
+            </li>
+            <li class="mx-4 md:mx-0 md:my-0 my-6">
+              <router-link to="login_register" class="text-base hover:text-primary duration-300">Contact US</router-link>
+            </li>
+            <li class="mx-4 md:mx-0 md:my-0 my-6">
+              <router-link to="/login_register" class="text-base bg-midPrimary hover:bg-primary hover:text-white rounded-md md:px-7 px-2 md:py-2 py-1 duration-300">
+                Login
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
