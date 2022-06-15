cc1c9faa96a31f362796ff3c07e0833ee7a5311f
virakkhun <rakmanoban@gmail.com>
2022-06-15 16:36:51 +0700

    remove: auth from home

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const routes = [
         component: userHome,
         meta: {
           title: "Home",
-          requireAuth: true,
+          requireAuth: false,
         },
       },
       {
